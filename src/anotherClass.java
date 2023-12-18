d15bafd37b772783ecb118513a163a6ca825280a
Fluffy881 <152864900+Fluffy881@users.noreply.github.com>
2023-12-18 15:15:02 +0100

    Update anotherClass.java

--- src/anotherClass.java
+++ src/anotherClass.java
@@ -1,6 +1,6 @@
 public class anotherClass {
     public static void main(String[] args) {
         System.out.println("Conflict solved");
-        System.out.println("Nouvelle modif");
+        System.out.println("Nouveau conflit");
     }
 }
