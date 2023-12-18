935683be798a1b377f7cc249352aeb0a71ca88a0
Fluffy881 <baptisteriff881@gmail.com>
2023-12-18 16:13:28 +0100

    fix : cleanning

--- src/anotherClass.java
+++ src/anotherClass.java
@@ -4,5 +4,6 @@ public class anotherClass {
         System.out.println("Nouveau conflit resolu");
         System.out.println("Nouvelle modification");
         System.out.println("for tags");
+        System.out.println("cleanning");
     }
 }
